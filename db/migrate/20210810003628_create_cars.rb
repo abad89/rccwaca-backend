ac3adbe87233c6dfe1fac27e9b0b844b3d56e76a
abad89 <abadalamenti89@gmail.com>
2021-08-09 23:12:39 -0400

    add more columns to cars migration

--- db/migrate/20210810003628_create_cars.rb
+++ db/migrate/20210810003628_create_cars.rb
@@ -2,6 +2,9 @@ class CreateCars < ActiveRecord::Migration[5.2]
   def change
     create_table :cars do |t|
       t.string :name
+      t.string :manufacturer
+      t.integer :price
+      t.string :img_url
     end
   end
 end
